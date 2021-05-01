6a03ae4849b5d335dfbc1e6e82c1917f733d7cab
Ben Cordero <bencord0@condi.me>
2021-05-01 23:16:07 +0100

    scripts/livecd-stage2: Enable dynamic networking on boot

--- scripts/livecd-stage2.sh
+++ scripts/livecd-stage2.sh
@@ -6,4 +6,23 @@ set -ex
 sed -i '/^root:/s/:\*:/::/' /etc/shadow
 echo ttyS0 >> /etc/securetty
 
+# Autoconfigure network from DHCP
+cat << NETWORK > /etc/systemd/network/wired.network
+[Match]
+Type=ether
+
+[Network]
+DHCP=both
+NETWORK
+
+cat << NETWORK > /etc/systemd/network/wireless.network
+[Match]
+Type=wlan
+
+[Network]
+DHCP=both
+NETWORK
+
+systemctl enable systemd-networkd systemd-resolved
+
 exit 0
